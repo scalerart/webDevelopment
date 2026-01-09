af2347602ab99ef991af7b2bfbc9cf9eeee7ff3d
scalerart <info.scalerart@gmail.com>
2026-01-09 13:01:03 +0600

    javaScript Introduction File Add

---
+++ javaScript/01.Introduction.js
@@ -0,0 +1,71 @@
+/*
+আজকের Final Checklist
+    ✔ JavaScript কী
+    ✔ কেন দরকার
+    ✔ কোথায় ব্যবহার হয়
+    ✔ console.log()
+    ✔ Internal & External JS
+    ✔ Comment
+    ✔ Console ব্যবহার
+
+=>  JavaScript কী?
+    JavaScript (JS) হলো একটি Programming Language যা ওয়েবসাইটকে জীবন্ত (Dynamic) করে তোলে।
+
+    =>  HTML = কাঠামো
+    =>  CSS = সাজসজ্জা
+    =>  JavaScript = কাজ করার ক্ষমতা (Brain)
+    উদাহরণ:
+        1.  বাটনে ক্লিক করলে কিছু হওয়া
+        2.  ফর্ম ভ্যালিডেশন
+        3.  লাইভ সার্চ
+        4.  স্লাইডার, মডাল, ডার্ক মোড
+        =>  সবই JavaScript দিয়ে হয়
+
+=>  JavaScript কোথায় ব্যবহার হয়?
+    JavaScript শুধু ওয়েবেই না—সব জায়গায়!
+
+    =>  Frontend
+        1.  Website Interaction
+        2.  React, Vue, Angular
+
+    =>  Backend
+        1.  Node.js দিয়ে Server বানানো
+        2.  API তৈরি
+
+    =>  Mobile App
+        1.  React Native
+        2.  Ionic
+
+    =>  Desktop App
+        1.  Electron (VS Code, WhatsApp)
+
+    =>  Game Development
+        3.  Browser Game
+        4.  Canvas Game
+        
+=>  console.log() কী?
+    console.log() দিয়ে আমরা কোডের আউটপুট দেখি।
+    উদাহরণ:
+        1.  console.log("Hello JavaScript");
+        2.  Output দেখাবে Browser Console এ
+        3.  Debug করার জন্য সবচেয়ে গুরুত্বপূর্ণ জিনিস
+
+=>  Internal JavaScript
+    HTML ফাইলের ভিতরে <script> ট্যাগ ব্যবহার করে JS লিখলে তাকে Internal JS বলে।
+    =>  ছোট প্রজেক্ট বা টেস্টিং-এর জন্য ভালো
+
+=>  External JavaScript
+    আলাদা .js ফাইল বানিয়ে HTML এর সাথে যুক্ত করলে তাকে External JS বলে।
+    =>  বড় প্রজেক্টে সবসময় External JS ব্যবহার করা হয়
+
+=>  JavaScript Comment
+    Comment হলো এমন লেখা যা কোড রান হয় না—শুধু মানুষের জন্য।
+    1.  Single Line Comment
+        // This is a Single Line Comment
+    2.  Multi Line Comment
+        /* This is multi line comment
+    =>  Comment ব্যবহার হয়:
+        1.  কোড বোঝার জন্য
+        2.  Future reference
+        3.  Debug করার সময়
+*/    
